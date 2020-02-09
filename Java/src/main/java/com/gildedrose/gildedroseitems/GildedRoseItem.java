cbd3b71163844c5d4de92be0dc077ab42ee59e9c
denshade <lieven.vaneeckhaute@gmail.com>
2020-02-09 16:15:58 +0100

    Check the item for npe & above saturation quality.

--- Java/src/main/java/com/gildedrose/gildedroseitems/GildedRoseItem.java
+++ Java/src/main/java/com/gildedrose/gildedroseitems/GildedRoseItem.java
@@ -7,7 +7,17 @@ abstract public class GildedRoseItem
     private static final int SATURATIONMAX = 50;
     protected final Item item;
 
+    /**
+     *
+     * @param item item to be considered as a gilder rose item. Must not have name == null
+     */
     public GildedRoseItem(Item item) {
+        if (item.name == null){
+            throw new IllegalArgumentException("Name must not be empty");
+        }
+        if (item.quality > SATURATIONMAX){
+            throw new IllegalArgumentException("item quality must not be bigger than " + SATURATIONMAX + " actual: " + item.quality);
+        }
         this.item = item;
     }
 
